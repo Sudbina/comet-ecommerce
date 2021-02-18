49eb34e5f6927e736cd294f4a450376616170eea
Jack Gibson <enozorez@gmail.com>
2021-02-18 12:24:10 +0000

    update getUserProfile function to return user details

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,7 +27,19 @@ const userAuth = asyncHandler(async (req, res) => {
 // @route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('Success');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 export { userAuth, getUserProfile };
