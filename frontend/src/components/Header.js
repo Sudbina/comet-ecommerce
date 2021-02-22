635afa51cc6969752e15b585550630ac6e2fd5ec
Jack Gibson <enozorez@gmail.com>
2021-02-22 11:30:15 +0000

    Add login screen, form container component and edit react router route for login

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
+
 const Header = () => {
+  const userInfo = useSelector((state) =>
+    state.userLogin.userInfo ? state.userLogin.userInfo : null
+  );
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' collapseOnSelect expand='lg'>
@@ -17,11 +23,17 @@ const Header = () => {
                   <i className='fas fa-shopping-cart'></i> Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <i className='fas fa-user'></i> Sign In
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <Navbar.Text>
+                  <i className='fas fa-user'></i> Hi {userInfo.name}!
+                </Navbar.Text>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user'></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
